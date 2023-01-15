b64f6663013eb93f0d1a5494445c882690bdbba3
Yifan <zhangyf_sjtu@sjtu.edu.cn>
2023-01-15 15:10:37 +0800

    Update scrape_filename.py
    update to py3

--- scrape_filename.py
+++ scrape_filename.py
@@ -25,14 +25,14 @@ def get_params():
 
 params=get_params()
 
-action=urllib.unquote_plus(params["action"])
+action=urllib.parse.unquote_plus(params["action"])
 
 if action == 'find':
-    title=urllib.unquote_plus(params["title"])
+    title=urllib.parse.unquote_plus(params["title"])
     liz=xbmcgui.ListItem(title, thumbnailImage='DefaultVideo.png', offscreen=True)
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=title, listitem=liz, isFolder=True)
 elif action == 'getdetails':
-    url=urllib.unquote_plus(params["url"])
+    url=urllib.parse.unquote_plus(params["url"])
     liz=xbmcgui.ListItem(url, offscreen=True)
     liz.setInfo('video',
         {'title': url
